57552ac385f72c267627f05df8b38f6799bfa07a
GaziaKausar <gaziakausar@gmail.com>
2026-05-31 17:31:20 +0530

    Create user_list.php

---
+++ Assignment/Day5/user_list.php
@@ -0,0 +1,62 @@
+<?php
+include "db.php";
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn, $query);
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>User List</h2>
+
+<table border="1" cellpadding="10">
+
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Age</th>
+    <th>Action</th>
+</tr>
+
+<?php
+
+while($row = mysqli_fetch_assoc($result))
+{
+?>
+
+<tr>
+
+    <td><?php echo $row['id']; ?></td>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['email']; ?></td>
+    <td><?php echo $row['age']; ?></td>
+
+    <td>
+        <a href="user_edit.php?id=<?php echo $row['id']; ?>">
+            Edit
+        </a>
+
+        |
+
+        <a href="user_delete.php?id=<?php echo $row['id']; ?>">
+            Delete
+        </a>
+    </td>
+
+</tr>
+
+<?php
+}
+?>
+
+</table>
+
+<br>
+
+<a href="user_add.php">Add New User</a>
+
+</body>
+</html>
