0ef386dfec9cf92e166c617c6a3ec61757278c69
GaziaKausar <gaziakausar@gmail.com>
2026-06-05 17:03:22 +0530

    Create footer.php

---
+++ Assignment/Day15/include/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark text-white text-center py-3 mt-auto">
+        <div class="container">
+            <small>&copy; <?= date('Y'); ?> InternApp. All Rights Reserved.</small>
+        </div>
+    </footer>
+    <!-- Bootstrap 5 Bundle JS with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
